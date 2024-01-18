834d1834bab5de44a5a227ef3c9992c55095fb62
Vladimir Safronov <safron5000@mail.ru>
2024-01-18 13:00:04 +0300

    add hw1

---
+++ src/main/java/ru/safronov/mySpringProject/hw1/SpringBootSeminar1Application.java
@@ -0,0 +1,13 @@
+package ru.safronov.mySpringProject.hw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class SpringBootSeminar1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootSeminar1Application.class, args);
+  }
+
+}
